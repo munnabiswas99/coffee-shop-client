03ec276572976a97a0dd3bef5f5368548c11632a
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-02-22 17:48:05 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddCoffee = () => {
+  return (
+    <div className="p-24">
+      <div className="p-12 text-center space-x-4">
+        <h1 className="text-6xl font-bold">Add Coffee</h1>
+        <p className="text-gray-600">
+          It is a long established fact that a reader will be distraceted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using Content here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AddCoffee;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import MainLayout from './layouts/MainLayout.jsx';
+import Home from './components/Home.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+let router = createBrowserRouter([
+  {  
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: "addCoffee",
+        Component: AddCoffee
+      },
+      {
+        path: "updateCoffee",
+        Component: UpdateCoffee
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
